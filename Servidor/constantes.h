b47a168aed083a1c588c6c6079faad58895cd707
Filipe Carvalho <ftcarvalho@gmail.com>
2024-03-30 12:28:03 +0000

    estruturas

--- Servidor/constantes.h
+++ Servidor/constantes.h
@@ -1,20 +1,74 @@
 #pragma once
-typedef struct Empresa Empresa, pEmpresa;
+#include <stddef.h>
+
+#define MAX_EMPRESAS 5 //var ambiente dps
+
+// TODO: alterar os valores hardcoded dos tamanhos dos arrays para outros valores
+
+// Estrutura que representa informações gerais de uma empresa
+typedef struct Empresa Empresa, * pEmpresa;
 struct Empresa {
-	char nome[50];
-	unsigned long quantidadeAcoes;
-	unsigned long valorAcao;
+    char nome[50];
+    unsigned long quantidadeAcoes;
+    double valorAcao;
 };
 
+// Estrutura que representa um utilizador do sistema
 typedef struct Utilizador Utilizador, * pUtilizador;
 struct Utilizador {
-	char nome[50];
-	char password[50];
-	unsigned long saldo;
+    char username[50];
+    char password[50];
+    unsigned long saldo;
 };
 
+// Estrutura que representa a relação entre uma empresa e as ações que um utilizador tem dessa empresa
 typedef struct EmpresaAcao EmpresaAcao, * pEmpresaAcao;
 struct EmpresaAcao {
-	char nomeEmpresa[50];
-	unsigned long quantidadeAcoes;
+    char nomeEmpresa[50];
+    unsigned long quantidadeAcoes;
 };
+
+// Estrutura que detalha transação de compra ou venda
+typedef struct DetalhesTransacao DetalhesTransacao, * pDetalhesTransacao;
+struct DetalhesTransacao {
+    TipoTransacao TipoT;
+    char nomeEmpresa[50];
+    unsigned long quantidadeAcoes;
+    double precoPorAcao;
+};
+
+typedef enum {
+    COMPRA,
+    VENDA
+} TipoTransacao;
+
+typedef enum {
+    LOGIN,
+    LOGOUT,
+    COMPRA,
+    VENDA,
+    LISTA_EMPRESAS,
+    VERIFICA_SALDO,
+    RESPOSTA
+} TipoMensagem;
+
+// Estrutura para comunicação entre cliente e servidor
+typedef struct Mensagem {
+    TipoMensagem TipoM;
+    char data[256];
+};
+
+/*// Estrutura que representa a carteira de ações de um utilizador
+typedef struct CarteiraAcoes {
+    EmpresaAcao acoes[MAX_EMPRESAS];
+    size_t numAcoes;
+} CarteiraAcoes;
+
+
+typedef struct HistoricoTransacao {
+    TipoTransacao tipo;      // COMPRA ou VENDA
+    char nomeEmpresa[50];
+    DWORD quantidadeAcoes;
+    double precoTransacao;
+} HistoricoTransacao;
+*/
